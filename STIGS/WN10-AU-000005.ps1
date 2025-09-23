ee671dd3577eb9360ae81f4d67ea251ee1acea70
hsultana <hsultana97@yahoo.com>
2025-09-23 11:16:13 +0100

    Create WN10-AU-000005.ps1

---
+++ STIGS/WN10-AU-000005.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that Windows 10 account lockout duration must be configured to 15 minutes or greater.
+
+.NOTES
+    Author          : Habiba Sultana
+    LinkedIn        : linkedin.com/in/habiba-sultana-b074a32a0
+    GitHub          : github.com/hsultana97
+    Date Created    : 2025-09-23
+    Last Modified   : 2024-09-23
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000005 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AU-000005.ps1 
+#>
+
+#Account Lockout Policy Setup
+# =============================
+
+# Set account will lock after 5 invalid attempts
+net accounts /lockoutthreshold:5
+
+# Reset counter after 15 minutes
+net accounts /lockoutwindow:15
+
+# Set lockout duration to 15 minutes (or 0)
+net accounts /lockoutduration:15
+
+# =============================
+# Display the applied settings
+# =============================
+Write-Host "`nCurrent Account Lockout Policy:"
+net accounts
